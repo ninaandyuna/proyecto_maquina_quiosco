4f1254bb06122e4f36bbad1eae0e0642b8403323
adri <adri@localhost.localdomain>
2017-09-17 16:44:45 +0200

    a

---
+++ Ejemplos_practicos/Servicios/oneshot/backup-shutdown.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# backup-shutdown.sh
+# 16/09/2017
+# ----------------------------------------------------------------------
+
+tar czf /root/backup-home-adri.tar.gz /home/adri/Desktop/Bq_Aquaris_Aug_2017/Images_Windows
